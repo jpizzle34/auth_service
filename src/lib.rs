2c8c856e18557e319040c615fe5ba991f838ab28
John <johnnyz88@gmail.com>
2022-02-07 01:23:25 +1100

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code, unused_variables)]
+
+mod auth_utils;
+mod database;
+
+pub use auth_utils::models::Credentials;
+use database::Status;
+
+pub fn authenticate(creds: Credentials) {
+    if let Status::Connected = database::connect_to_database() {
+        auth_utils::login(creds);
+    }
+}
